0909b081c5da3246c3df01fd4434a5964c28505d
Sannidhi Mahajan <mahajansannidhi@gmail.com>
2026-03-12 19:17:09 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,28 +1,18 @@
-from flask import Flask, render_template, request, redirect
-from config import Config
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return redirect("/posts")
+    return render_template("posts.html")
 
 @app.route("/login")
 def login():
-    return "Login Page"
-
-@app.route("/posts")
-def posts():
-    return "Posts Page"
+    return render_template("login.html")
 
 @app.route("/create")
 def create():
-    return "Create Post Page"
-
-@app.route("/edit/<id>")
-def edit(id):
-    return f"Edit post {id}"
+    return render_template("edit.html")
 
 if __name__ == "__main__":
     app.run()
