d91c7bf411b60f1627e8c9d07e9ba60a58b2b599
Sannidhi Mahajan <mahajansannidhi@gmail.com>
2026-03-12 19:08:56 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
-def index():
-    return "CMS App Running on Azure"
+def home():
+    return redirect("/posts")
+
+@app.route("/login")
+def login():
+    return "Login Page"
+
+@app.route("/posts")
+def posts():
+    return "Posts Page"
+
+@app.route("/create")
+def create():
+    return "Create Post Page"
+
+@app.route("/edit/<id>")
+def edit(id):
+    return f"Edit post {id}"
 
 if __name__ == "__main__":
     app.run()
